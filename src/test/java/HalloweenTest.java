8310bf412268c54505754ce81adb7eb6e854838b
lalabayramova <b.lala95@yahoo.com>
2020-11-10 13:45:08 -0500

    no trick or treat commit

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -1,3 +1,9 @@
 public class HalloweenTest {
     // A am about to give everyone candy for halloween
+    // are we there yet ???? Hello Hello
+    // lets play a game
+    // it is fun to drive so far
+
+    // There will be no treat amd trick this year
+    // stay home and be safe  and wear your mask outside
 }
